2dab59aecfd189b99815450ead1d222c63db2be8
JoshuaPhee <pheej@uci.edu>
2025-10-26 23:33:50 -0700

    longest activity by weekday or weekend

--- js/activities.js
+++ js/activities.js
@@ -22,6 +22,7 @@ function parseTweets(runkeeper_tweets) {
 	document.getElementById("longestActivityType").innerText = findLongestDistanceObject(tweet_array, objectActivities);
 	document.getElementById("shortestActivityType").innerText = findShortestDistanceObject(tweet_array, objectActivities);
 
+	document.getElementById("weekdayOrWeekendLonger").innerText = findLongDay(tweet_array);
 	//TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
 
 	activity_vis_spec = {
@@ -38,6 +39,32 @@ function parseTweets(runkeeper_tweets) {
 	//Use those visualizations to answer the questions about which activities tended to be longest and when.
 }
 // in one loop, sort and gather different activity types tracking their count and average 
+function findLongDay(tweet_array) {
+	var weekdayCount = 0;
+	var weekdayTotalDis = 0;
+	var weekendCount = 0;
+	var weekendTotalDis = 0;
+
+	for (let i = 0; i < tweet_array.length; i++) {
+		if (tweet_array[i].time.getDay() == 0 || tweet_array[i].time.getDay() == 6) {
+			weekendCount += 1;
+			weekendTotalDis += tweet_array[i].distance;
+		}
+		else {
+			weekdayCount += 1;
+			weekdayTotalDis += tweet_array[i].distance;
+		}
+	}
+	var weekendAvg = weekendTotalDis/weekendCount;
+	var weekdayAvg = weekdayTotalDis/weekdayCount;
+	if (weekendAvg > weekdayAvg) {
+		return "weekends";
+	}
+	else {
+		return "weekdays";
+	}
+}
+
 function findLongestDistanceObject(tweet_array, objectActivities) {
 	// console.log("LONGEST")
 	var longAct = "";
