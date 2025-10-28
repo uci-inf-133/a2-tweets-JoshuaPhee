9ce44d90e6fa9f9aa25ebb45746ffb0494331db7
JoshuaPhee <pheej@uci.edu>
2025-10-27 22:36:47 -0700

    Completed search interface

--- js/descriptions.js
+++ js/descriptions.js
@@ -13,25 +13,71 @@ function parseTweets(runkeeper_tweets) {
 		}
 	}
 	// console.log(writtenArray);
+	return writtenArray;
 }
 
 
-function addEventHandlerForSearch() {
+function addEventHandlerForSearch(writtenArray) {
 	//TODO: Search the written tweets as text is entered into the search box, and add them to the table
 	document.getElementById("textFilter").addEventListener("input", function() {
 		var checkInput = document.getElementById("textFilter").value;
-		console.log(checkInput);
-		
+		document.getElementById("searchText").innerText = checkInput;
 
+		if (checkInput === "") {
+			document.getElementById("tweetTable").innerHTML = "";
+			document.getElementById("searchCount").innerText = 0;
+		}
+		else {
+			document.getElementById("searchCount").innerText = 0;
+			document.getElementById("tweetTable").innerHTML = "";
+			var count = 1;
+			for (let i = 0; i<writtenArray.length; i++) {
+				if (writtenArray[i].written && writtenArray[i].activityType !== "unknown") {
+					if (writtenArray[i].text.includes(checkInput)) {
+						var getTable = document.getElementById("tweetTable");
+						var newRow = document.createElement("tr");
 
+						var rowNum = document.createElement("td");
+						rowNum.scope = "row";
+						rowNum.textContent = count;
 
+						var rowActivity = document.createElement("td");
+						rowActivity.textContent = writtenArray[i].activityType;
+
+						const urlregex = /https:\/\/[^\s]+/;
+						var rowText = document.createElement("td");
+						let tweetText = writtenArray[i].text;
+						tweetText = tweetText.replace(urlregex, (url) => {
+							return `<a href="${url}" ${url}</a>`
+						})
+
+						rowText.innerHTML = tweetText;
+
+						newRow.append(rowNum);
+						newRow.append(rowActivity);
+						newRow.append(rowText);
+
+						getTable.appendChild(newRow);
+						count += 1;
+					}
+				}
+			}
+			document.getElementById("searchCount").innerText = count - 1;
+		}
 
 	})
 	
 }
 
+function checkNumTweets(text) {
+	var count = 0;
+
+	return count;
+}
+
 //Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function (event) {
-	addEventHandlerForSearch();
-	loadSavedRunkeeperTweets().then(parseTweets);
+	document.getElementById("searchCount").innerText = 0;
+	document.getElementById("searchText").innerText = "";
+	loadSavedRunkeeperTweets().then(parseTweets).then(addEventHandlerForSearch);
 });
